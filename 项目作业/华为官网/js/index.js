7832b273704aee42e30299668b6eacdbde5c399f
明心 <1320959793@qq.com>
2021-05-18 23:20:33 +0800

    1322323

--- 项目作业/华为官网/js/index.js
+++ 项目作业/华为官网/js/index.js
@@ -1,8 +1,6 @@
 $('.xfzyw').click(function(){
     $('.xfz-con').slideToggle(300)
-    $('.xfzyw::after').animate({
-        rotate:360 ,
-    })
+    $('.xfzyw span').toggleClass('active');
 })
 $('.nav-lis').hover(function(){
     $(this).css('color','#999')
@@ -10,4 +8,19 @@ $('.nav-lis').hover(function(){
     $(this).css('color','#000')
 })
 
-$()
+//点击 颜色选项
+let x = 0;
+$('.color-item').eq(x).css('display','block');
+$('.color-select').eq(x).css('display','block')
+$('.color-button').click(function(){
+    $('.color-button').children().removeClass('active');
+    $(this).children().addClass('active');
+    let dex = $(this).parent().index();
+    $('.color-item').eq(x).css('display','none');
+    $('.color-item').eq(dex).css('display','block')
+    $('.color-select').eq(x).css('display','none');
+    $('.color-select').eq(dex).slideToggle(1000);
+    x = dex;
+    // let title = $(this).index()].title;
+    // $('.color-title').html(title);
+}) 
